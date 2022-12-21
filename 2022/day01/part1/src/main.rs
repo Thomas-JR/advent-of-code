6e040419340a10d59a99abcc97748ca0a5f2d6a1
Thomas Robertson <thomasrobertson24@gmail.com>
2022-12-21 17:07:16 +1100

    2022/day 1-all

--- 2022/day01/part1/src/main.rs
+++ 2022/day01/part1/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::read_to_string;
+use std::cmp;
+
 fn main() {
-    println!("Hello, world!");
+    // let file = read_to_string("../test-input.txt").expect("Unable to read file");
+    let file = read_to_string("../input.txt").expect("Unable to read file");
+
+    let mut max = 0;
+    let mut cur = 0;
+    for line in file.lines() {
+        if line == "" {
+            max = cmp::max(max, cur);
+            cur = 0;
+            continue;
+        }
+        cur += line.parse::<u32>().unwrap();
+    }
+
+    println!("max = {}", max);
 }
