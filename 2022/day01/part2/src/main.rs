5f25b46f6d1824828d93218b114eb8e7c3bd9fc5
Thomas Robertson <thomasrobertson24@gmail.com>
2022-12-21 17:06:01 +1100

    2022/day 1

--- 2022/day01/part2/src/main.rs
+++ 2022/day01/part2/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    // let file = read_to_string("../test-input.txt").expect("Unable to read file");
+    let file = read_to_string("../input.txt").expect("Unable to read file");
+
+    let mut v: Vec<u32> = Vec::new();
+    let mut cur = 0;
+    for line in file.lines() {
+        if line == "" {
+            v.push(cur);
+            cur = 0;
+            continue;
+        }
+        cur += line.parse::<u32>().unwrap();
+    }
+
+    v.sort();
+    v.reverse();
+
+    if v.len() < 3 {
+        panic!("Expected more inputs than {}", v.len());
+    }
+
+    println!("sum = {}", v[0] + v[1] + v[2]);
 }
