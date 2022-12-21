ea16679e0b2de3ddb496cd31b6fa49181b7a7e22
Thomas Robertson <thomasrobertson24@gmail.com>
2022-12-21 17:37:56 +1100

    2022/day-4 rust

--- 2022/day04/part1/src/main.rs
+++ 2022/day04/part1/src/main.rs
@@ -1,7 +1,11 @@
+use std::fs::read_to_string;
+
 fn main() {
+    let file = read_to_string("../input.txt").expect("Unable to read file");
+
     let mut count = 0;
    
-    for line in INPUT.lines() {
+    for line in file.lines() {
         let ranges: Vec<&str> = line.split(',').collect();
         let f = ranges[0];
         let s = ranges[1];
@@ -15,11 +19,8 @@ fn main() {
         let s0 = s_ranges[0].parse::<i32>().unwrap();
         let s1 = s_ranges[1].parse::<i32>().unwrap();
        
-        // println!("{f0}-{f1},{s0}-{s1}");
-       
         if f0 <= s0 && f1 >= s1 || s0 <= f0 && s1 >= f1 {
             count += 1;
-            // println!("found");
         }
     }
    
