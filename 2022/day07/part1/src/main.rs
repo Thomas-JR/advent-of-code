ef389db0e32a7eb83eafea88339b52b03030fbdc
Thomas Robertson <thomasrobertson24@gmail.com>
2022-12-24 15:10:56 +1100

    2022/day 7 rust scaffolding

--- 2022/day07/part1/src/main.rs
+++ 2022/day07/part1/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let file = read_to_string("../test-input.txt").expect("failed to read file");
+    for line in file.lines() {
+        let mut chars = line.chars();
+
+        println!("{line}");
+        if chars.nth(0).expect("invalid string in line") == '$' {
+            continue;
+        }
+        println!("  yes");
+        // if line.chars()[0] == '$' {
+        //     println!("action");
+        // } else {
+        //     println!("file");
+        // }
+    }
 }
